7a20acfce84670e6161db54e51a61a0352c0d9e3
David Samson <david.andrew.engineer@gmail.com>
2025-08-30 01:24:33 -0400

    got parenthesis working!

--- test_np_pratt.py
+++ test_np_pratt.py
@@ -14,21 +14,24 @@ TBD problems to solve
 import numpy as np
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
-from typing import Callable, dataclass_transform, Literal
+from typing import Callable, dataclass_transform, Literal, cast
 from enum import Enum, auto
 
 
 import pdb
 
 
-type OperatorLiteral = Literal["^", "*", "/", "+", "-"]
+type OperatorLiteral = Literal["^", "*", "/", "+", "-", ";"]
+type GroupLiteral = Literal["(", ")", "{", "}", "[", "]"]
 class Assoc(Enum):
+    # binops
     left = auto()
     right = auto()
+    none = auto()
+
+    # unary ops
     prefix = auto()
     postfix = auto()
-    none = auto()
-    group = auto()
 
 
 @dataclass_transform()
@@ -53,9 +56,16 @@ class OperatorT(Token):
         return self.value
 
 class GroupT(Token):
-    value: Literal["(", ")", "{", "}", "[", "]"]
+    value: GroupLiteral
     def __str__(self) -> str:
         return self.value
+
+# unfortunately, python typing doesn't support str[GroupLiteral], i.e. string made up only of GroupLiteral values
+group_matchers: dict[GroupLiteral, str] = {
+    '[': '])',
+    '(': '])',
+    '{': '}'
+}
 
 class WhitespaceT(Token):
     ...
@@ -115,7 +125,7 @@ def eat_number(src:SourceBox) -> NumberT|None:
     return None
 
 def eat_operator(src:SourceBox) -> OperatorT|None:
-    if src.s[0] in "+-*/^":
+    if src.s[0] in "+-*/^;":
         op = src.s[0]
         src.s = src.s[1:]
         return OperatorT(value=op)
@@ -158,22 +168,23 @@ optable: list[list[tuple[OperatorLiteral, Assoc]]] = [
     [("*", Assoc.left), ("/", Assoc.left)],
     [("+", Assoc.left), ("-", Assoc.left)],
     [(';', Assoc.postfix)],
-    [('(', Assoc.group), (')', Assoc.group), ('{', Assoc.group), ('}', Assoc.group), ('[', Assoc.group), (']', Assoc.group)]
 ]
+
+binops: set[OperatorLiteral] = {op for row in optable for op, assoc in row if assoc in (Assoc.right, Assoc.left, Assoc.none)}
+prefixes: set[OperatorLiteral] = {op for row in optable for op, assoc in row if assoc == Assoc.prefix}
+postfixes: set[OperatorLiteral] = {op for row in optable for op, assoc in row if assoc == Assoc.postfix}
 
 BASE_BIND_POWER = 1   #TBD, but 0 probably for groups ()
 NO_BIND = -1
-def left_bp(i: int) -> tuple[int, int]: 
+def left_bp(i: int) -> tuple[int, int]:
     return (BASE_BIND_POWER+2*i, BASE_BIND_POWER+2*i+1)
-def right_bp(i: int) -> tuple[int, int]: 
+def right_bp(i: int) -> tuple[int, int]:
     return (BASE_BIND_POWER+2*i+1, BASE_BIND_POWER+2*i)
-def prefix_bp(i: int) -> tuple[int, int]: 
+def prefix_bp(i: int) -> tuple[int, int]:
     return (BASE_BIND_POWER+2*i, NO_BIND)
-def postfix_bp(i: int) -> tuple[int, int]: 
+def postfix_bp(i: int) -> tuple[int, int]:
     return (NO_BIND, BASE_BIND_POWER+2*i)
-def group_bp(i: int) -> tuple[int, int]: 
-    return (NO_BIND, NO_BIND)
-def none_bp(i: int) -> tuple[int, int]: 
+def none_bp(i: int) -> tuple[int, int]:
     return (NO_BIND, NO_BIND)
 
 bp_funcs: dict[Assoc, Callable[[int], tuple[int, int]]] = {
@@ -181,7 +192,6 @@ bp_funcs: dict[Assoc, Callable[[int], tuple[int, int]]] = {
     Assoc.right: right_bp,
     Assoc.prefix: prefix_bp,
     Assoc.postfix: postfix_bp,
-    Assoc.group: group_bp,
     Assoc.none: none_bp,
 }
 
@@ -198,7 +208,7 @@ class AST(ABC):
     def __init_subclass__(cls: 'type[AST]', **kwargs) -> None:
         super().__init_subclass__(**kwargs)
         dataclass(cls, repr=False)
-    
+
     @abstractmethod
     def eval(self) -> int|float: ...
 
@@ -206,6 +216,22 @@ class Atom(AST):
     val: int|float
     def __repr__(self) -> str: return str(self.val)
     def eval(self) -> int|float: return self.val
+
+
+class SeqGroup(AST):
+    items: list[AST]
+    def __repr__(self):
+        items_str = ', '.join([str(i) for i in self.items])
+        return f'({items_str})'
+    def eval(self) -> list[int|float]: return [i.eval() for i in self.items]
+
+class Group(AST):
+    item: AST
+    def __repr__(self):
+        return f'({self.item})'
+    def eval(self):
+        return self.item.eval()
+
 
 @dataclass
 class BinOp(AST):
@@ -215,7 +241,7 @@ class BinOp(AST):
 
     def __repr__(self) -> str:
         return f"({self.op} {self.left} {self.right})"
-    
+
     def eval(self) -> int|float:
         left = self.left.eval()
         right = self.right.eval()
@@ -233,35 +259,139 @@ class BinOp(AST):
         else:
             raise ValueError(f"unknown operator: '{op}'. {self=}")
 
+@dataclass
+class PrefixOp(AST):
+    def eval(self): raise NotImplementedError
+@dataclass
+class PostfixOp(AST):
+    def eval(self): raise NotImplementedError
+
+
+def reduce_ops(tokens: list[Token|AST]) -> int:
+    ast_idxs = [i for i, t in enumerate(tokens) if isinstance(t, AST)]
+    # for every AST, get the right_bp of the left thing, and the left_bp of the right thing
+    adjacent_bps = []
+    for idx in ast_idxs:
+        left_t = tokens[idx-1] if idx-1>=0 else None
+        right_t = tokens[idx+1] if idx+1<len(tokens) else None
+        left_bp = -1
+        right_bp = -1
+        if left_t is not None and isinstance(left_t, OperatorT):
+            _, left_bp = bindpow[left_t.value]
+        if right_t is not None and isinstance(right_t, OperatorT):
+            right_bp, _ = bindpow[right_t.value]
+        adjacent_bps.append((left_bp, right_bp))
+
+    adjacent_bps = np.array(adjacent_bps)
+    diffs = np.diff(adjacent_bps)
+    shift_mask = np.any(adjacent_bps > 0, axis=1) * (diffs != 0)[:,0]
+    shift_dirs = np.sign(diffs)[:,0]
+
+
+    shift_to = ast_idxs + shift_dirs
+
+    # apply all the reductions
+    reductions_applied = 0
+    i = len(ast_idxs) - 1
+    while i >= 0:
+        if not shift_mask[i]:
+            i -= 1
+            continue
+        idx = ast_idxs[i]
+        ast = tokens[idx]
+        assert isinstance(ast, AST), f"INTERNAL ERROR: expected ast to be an AST. got {ast}"
+        target = shift_to[i]
+        target_tok = tokens[target]
+        assert isinstance(target_tok, (OperatorT)), f"INTERNAL ERROR: expected operator at target. got {tokens[target]}"  # (BinOp, PrefixOp, PostfixOp)
+        if target_tok.value in binops:
+            if idx < target:
+                # right item was not ready yet
+                i-=1
+                continue
+            elif idx > target:
+                if i-1 < 0 or shift_to[i-1] != target:
+                    # left item not ready or captured by the binop
+                    i-=1
+                    continue
+                tokens[target-1:target+2] = [BinOp(*tokens[target-1:target+2])]
+                i-=2
+                reductions_applied += 1
+                continue
+            else:
+                raise ValueError(f"INTERNAL ERROR: expected ast target idx to be different from ast's own idx. both are `{idx}`")
+        else:
+            raise NotImplementedError(f'need to handle non-binops. current was {ast=}, {target_tok=}')
+
+
+    return reductions_applied
+
+def make_groups(tokens: list[Token|AST]) -> int:
+    reductions_applied = 0
+    group_idxs = [i for i, t in enumerate(tokens) if isinstance(t, GroupT)]
+    i = len(group_idxs) - 1
+    while i >= 1:
+        left_idx = group_idxs[i-1]
+        right_idx = group_idxs[i]
+        # all items between left and right should be ASTs
+        if any(not isinstance(t, AST) for t in tokens[left_idx+1:right_idx]):
+            # interior not fully reduced
+            i -= 1
+            continue
+        left_t = tokens[left_idx]
+        try:
+            right_t = tokens[right_idx]
+        except:
+            pdb.set_trace()
+            ...
+        assert isinstance(left_t, GroupT), f'INTERNAL ERROR: left_t should be a GroupT. got {left_t}'
+        assert isinstance(right_t, GroupT), f'INTERNAL ERROR: right_t should be a GroupT. got {right_t}'
+
+        if left_t.value not in group_matchers:
+            i -= 1
+            continue
+            # raise ValueError(f'INTERNAL ERROR: GroupT token not in  group_matchers (i.e. need to expand it... {left_t.value=}')
+        if right_t.value not in group_matchers[left_t.value]:
+            # left and right are not a match
+            i -= 1
+            continue
+
+        # left and rigth match, and interior fully match!
+        if right_idx - left_idx > 2:
+            reductions_applied += 1
+            tokens[left_idx:right_idx+1] = [SeqGroup(tokens[left_idx+1:right_idx])]
+            i -= 2
+        elif right_idx - left_idx == 2:
+            reductions_applied += 1
+            tokens[left_idx:right_idx+1] = [Group(tokens[left_idx+1])]
+            i -= 2
+        else:
+            raise ValueError(f"left and right group index don't have any interior ASTs. {tokens[left_idx:right_idx+1]=}")
+
+    return reductions_applied
+
 def shunt_tokens(tokens: list[Token]) -> list[AST]:
+    tokens: list[Token|AST] = cast(list[Token|AST], tokens)
     for i, t in enumerate(tokens):
         if isinstance(t, NumberT):
             tokens[i] = Atom(t.value)
-    
+
     # TODO: probably just redo this as imperative loop (i.e. remove vectorization)
-    while True: #any(isinstance(t, OperatorT) for t in tokens):
-        op_indices = np.array([i for i, t in enumerate(tokens) if isinstance(t, (OperatorT, GroupT))])
-        left_pad = [] if isinstance(tokens[0], (OperatorT, GroupT)) else [NO_BIND, NO_BIND]
-        right_pad = [] if isinstance(tokens[len(tokens)-1], (OperatorT, GroupT)) else [NO_BIND, NO_BIND]
-        bind_pows = np.array(left_pad + [i for t in tokens if isinstance(t, (OperatorT, GroupT)) for i in bindpow[t.value]] + right_pad)[1:-1]
-        bind_pows = bind_pows.reshape(len(bind_pows)//2, 2)
-        pdb.set_trace()
-        shunt_dir = np.sign(np.diff(bind_pows))[..., 0]
-        # shunt_dir = (bind_pows[:,0] > bind_pows[:,1]).astype(int)
+    while True:
+        init_len = len(tokens)
+        reductions_applied = reduce_ops(tokens)
+        if reductions_applied == 0:
+            reductions_applied += make_groups(tokens)
+        # if reductions_applied == 0:
+        #     reductions_applied += make_flows(tokens)
+        # if reductions_applied == 0:
+        #     break
+        if len(tokens) == init_len:
+            # no reductions applied by anything
+            break
 
-        # anywhere there is a false followed by a true
-        group_indices = np.where(np.diff(shunt_dir) == 1)[0] + 1
-
-        # apply reductions (reverse order so the indices don't get messed up)
-        for i in reversed(group_indices):
-            op_idx = op_indices[i-1]  # -1 because atom, not operator on left or right side of expr. TODO: better handling of this
-            tokens[op_idx-1:op_idx+2] = [BinOp(*tokens[op_idx-1:op_idx+2])]
-        
-        pdb.set_trace()
-     
     if not all(isinstance(t, AST) for t in tokens):
         raise ValueError(f"token shunting failed. {tokens=}")
-    
+
     return tokens
 
 
@@ -272,8 +402,9 @@ def parse(tokens: list[Token]) -> list[AST]:
     return shunt_tokens(tokens)
 
 if __name__ == "__main__":
+
     DEBUG = True
-    
+
     if DEBUG: print(bindpow)
 
     from easyrepl import REPL
@@ -290,3 +421,9 @@ if __name__ == "__main__":
     # print(exprs:=shunt_tokens(tokenize(expr_str)))
     # for e in exprs:
     #     print(e.eval())
+
+
+test_cases = [
+    '(0)',
+    '(1+2)*3',
+]
