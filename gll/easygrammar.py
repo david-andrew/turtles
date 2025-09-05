92fca587ef39906ffa69f4da722dcff4bbb617e7
David Samson <david.andrew.engineer@gmail.com>
2025-09-05 15:32:53 -0400

    adding some ideas around getting typing to work correctly when instantiating

--- gll/easygrammar.py
+++ gll/easygrammar.py
@@ -1,4 +1,4 @@
-from typing import dataclass_transform
+from typing import dataclass_transform, Self, final
 from abc import ABC, ABCMeta
 import inspect
 import ast
@@ -17,11 +17,31 @@ class RuleMeta(ABCMeta):
         return cls
     def __ror__(cls: 'type[Rule]', other: 'type[Rule]|str|tuple') -> 'type[Rule]':
         return cls | other
+    
+
+    def __call__(cls, raw: str, /) -> Self:
+        # TODO: whole process of parsing the input string
+        # TODO: would it be possible to pass in a generic sequence (e.g. list[Token]), i.e. easy ability to separate scanner and parser?
+        # print(f"Custom call on classes: {cls.__name__} | {raw}")
+
+        # create an instance of the class (without calling __init__)
+        obj = cls.__new__(cls, cls.__name__)
+        
+        # define all of the members of the instance (based on the parse shape). e.g.
+        obj.a = 42  #DEBUG
+        obj.b = 43  #DEBUG
+        
+        return obj
 
 
-@dataclass_transform()
+# @dataclass_transform()
 class Rule(ABC, metaclass=RuleMeta):
     """initialize a token subclass as a dataclass"""
+    # this is just a placeholder for type-checking. The actual implementation is in the __call__ method.
+    @final
+    def __init__(self, raw:str, /):
+        ...
+
     @staticmethod
     def _collect_sequence_for_class(target_cls: type) -> list:
         """Return ordered (expr/decl) tuples found in the class body of target_cls."""
@@ -82,14 +102,24 @@ class Rule(ABC, metaclass=RuleMeta):
 
     def __init_subclass__(cls: 'type[Rule]', **kwargs) -> None:
         super().__init_subclass__(**kwargs)
+
+        # ensure that __init__ in this base class was not overridden
+        # TODO: can this point to where the __init__ was overridden?
+        if cls.__init__ != Rule.__init__:
+            raise ValueError(f"Rule subclass `{cls.__name__}` must not override __init__ in the base class.")
+
         # capture the ordered sequence of class-body expressions and declarations
         sequence = Rule._collect_sequence_for_class(cls)
         setattr(cls, "_sequence", sequence)
+
 
     # def __repr__(self) -> str:
     #     dict_str = ", ".join([f"{k}=`{v}`" for k, v in self.__dict__.items()])
     #     return f"{self.__class__.__name__}({dict_str})"
 
+
+# class Rule(RuleBase):
+#     def __init__(self, raw:str): ...
 
 
 
@@ -98,9 +128,26 @@ class ClassA(Rule):
     a:int
     ')'
 
+from typing import Annotated
+class Char:
+    def __init__(self, char: str): ...
+class Test(Rule):
+    a: Annotated[int, Char('('), Char(')')]
+    def __init__(self, a:int): ...
+a = Test('()')
+a.a
+
 class ClassB(Rule):
-    ClassA()
+    ClassA("(5)")
     b:str
+
+class ClassC:
+    '('
+    a: int
+    ')'
+
+c = ClassB('')
+c.b
 
 class t(Rule): ...
 
